511aed0f4f1da98823a3eecd823f5009849f34da
aggie-ch <aga.chorowiec@gmail.com>
2021-10-08 21:15:04 +0200

    feat: added database & hibernate configuration

--- src/main/java/io/github/aggie/ShopConfiguration.java
+++ src/main/java/io/github/aggie/ShopConfiguration.java
@@ -1,15 +1,31 @@
 package io.github.aggie;
 
+import com.zaxxer.hikari.HikariDataSource;
 import io.github.aggie.orders.OrderService;
 import io.github.aggie.payments.PaymentService;
 import io.github.aggie.products.ProductService;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.sql.DataSource;
+import java.util.Properties;
+
+
+@PropertySource("classpath:jdbc.properties")
 @EnableAspectJAutoProxy
+@EnableTransactionManagement
 @Configuration
 public class ShopConfiguration {
 
@@ -20,9 +36,40 @@ public class ShopConfiguration {
 
     @Bean
     public MessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        var messageSource = new ResourceBundleMessageSource();
         messageSource.setBasename("messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
+    }
+
+    @Bean
+    public DataSource dataSource(Environment environment) {
+        var dataSource = new HikariDataSource();
+        dataSource.setUsername(environment.getProperty("database.username"));
+        dataSource.setPassword(environment.getProperty("database.password"));
+        dataSource.setJdbcUrl(environment.getProperty("database.url"));
+        dataSource.setDriverClassName(environment.getProperty("database.driver"));
+        return dataSource;
+    }
+
+    @Bean
+    public PropertiesFactoryBean hibernateProperties() {
+        PropertiesFactoryBean factoryBean = new PropertiesFactoryBean();
+        factoryBean.setLocation(new ClassPathResource("hibernate.properties"));
+        return factoryBean;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory(DataSource dataSource, Properties hibernateProperties) {
+        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setHibernateProperties(hibernateProperties);
+        factoryBean.setPackagesToScan("io.github.aggie");
+        return factoryBean;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(SessionFactory sessionFactory) {
+        return new HibernateTransactionManager(sessionFactory);
     }
 }
--- src/main/java/io/github/aggie/payments/Payment.java
+++ src/main/java/io/github/aggie/payments/Payment.java
@@ -1,17 +1,32 @@
 package io.github.aggie.payments;
 
-import lombok.Builder;
-import lombok.Value;
+import io.github.aggie.common.FastMoneyUserType;
+import lombok.*;
+import org.hibernate.annotations.Columns;
+import org.hibernate.annotations.TypeDef;
 import org.javamoney.moneta.FastMoney;
 
+import javax.persistence.*;
 import java.time.Instant;
 
+@TypeDef(name = "fastMoney", typeClass = FastMoneyUserType.class, defaultForType = FastMoney.class)
+@Table(name = "payments", indexes = @Index(name = "payment_status", columnList = "status"))
+@Entity
 @Builder
-@Value
+@EqualsAndHashCode(exclude = "id")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Payment {
 
-    String id;
-    FastMoney money;
-    Instant timestamp;
-    PaymentStatus status;
+    @Id
+    private String id;
+    @Columns(columns = {
+            @Column(name = "currency", length = 3),
+            @Column(name = "value")
+    })
+    private FastMoney money;
+    private Instant timestamp;
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus status;
 }
