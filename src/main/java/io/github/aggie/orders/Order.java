cccdbf0772b2ad039a96c6e66796e1ec08ecdc83
aggie-ch <aga.chorowiec@gmail.com>
2021-10-08 21:43:39 +0200

    feat: added entities

--- src/main/java/io/github/aggie/orders/Order.java
+++ src/main/java/io/github/aggie/orders/Order.java
@@ -3,25 +3,34 @@ package io.github.aggie.orders;
 import io.github.aggie.payments.LocalMoney;
 import io.github.aggie.payments.Payment;
 import io.github.aggie.products.Product;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 import org.javamoney.moneta.FastMoney;
 
+import javax.persistence.*;
+import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.time.Instant;
 import java.util.List;
 
+@Table(name = "orders")
+@Entity
 @Data
+@EqualsAndHashCode(exclude = "id")
 @RequiredArgsConstructor
 @NoArgsConstructor
 public class Order {
 
+    @GeneratedValue
+    @Id
     private Long id;
+    @ManyToMany(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
     @NotEmpty
     @NonNull
     private List<Product> products;
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @Valid
     private Payment payment;
+    private Instant timestamp;
 
     public FastMoney getTotalPrice() {
         return products.stream()
--- src/main/java/io/github/aggie/products/Product.java
+++ src/main/java/io/github/aggie/products/Product.java
@@ -1,16 +1,30 @@
 package io.github.aggie.products;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
+import org.hibernate.annotations.Columns;
 import org.javamoney.moneta.FastMoney;
 
+import javax.persistence.*;
+
+@Table(name = "products", indexes = @Index(name = "product_type", columnList = "type"))
+@Entity
 @Builder
 @Data
+@EqualsAndHashCode(exclude = "id")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
 
+    @GeneratedValue
+    @Id
     private Long id;
     private String name;
     private String description;
+    @Columns(columns = {
+            @Column(name = "currency", length = 3),
+            @Column(name = "value")
+    })
     private FastMoney price;
+    @Enumerated(EnumType.STRING)
     private ProductType type;
 }
